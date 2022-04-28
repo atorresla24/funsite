9544aa7ec0c1cb1b59dbaf85de58eadb152a4a72
Alec Torres <atorresla24@gmail.com>
2022-04-27 23:13:21 -0400

    update

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 'use strict'
 
+let answer = prompt("Skateboards or surfboards");
+console.log(answer);
+
+let userName = prompt("What is your name");
+console.log(userName);
+
+if (answer == skateboards){
+    console.log("Good choice! WELCOME!");
+}else{
+    console.log("Well then this isn't the place for you...BYE!")
+    document.write("Well then this isn't the place for you...BYE!")
+}
