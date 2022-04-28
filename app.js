034442469e8adf5f62269672b75030a3a561143b
Alec Torres <atorresla24@gmail.com>
2022-04-28 10:55:57 -0400

    update

--- app.js
+++ app.js
@@ -9,6 +9,5 @@ console.log(userName);
 if (answer == skateboards){
     console.log("Good choice! WELCOME!");
 }else{
-    console.log("Well then this isn't the place for you...BYE!")
-    document.write("Well then this isn't the place for you...BYE!")
+    console.log("Well then this isn't the place for you...BYE!");
 }
