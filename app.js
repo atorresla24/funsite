58d05eaeb21729f44a815e30b6f3a975f727e123
Alec Torres <atorresla24@gmail.com>
2022-04-28 21:10:33 -0400

    update

--- app.js
+++ app.js
@@ -7,7 +7,32 @@ let userName = prompt("What is your name");
 console.log(userName);
 
 if (answer == skateboards){
-    console.log("Good choice! WELCOME!");
+    console.log("Good choice! WELCOME!" + userName);
 }else{
     console.log("Well then this isn't the place for you...BYE!");
 }
+
+let deckWidth = prompt('Skateboard width length (7.5in-8.7in');
+let respone;
+
+if(deckWidth < 8){
+    response = 'Guess small boards are for you!';
+ } else if(deckWidth == 8) {
+    response = 'Middle child much';
+ }else if(deckWidth < 8){
+    response = 'Look at this big man here!';
+ } else {
+    response = 'I guess skateboards are not for you :(';
+ }
+
+ let number;
+ let guesses = 3;
+ for(let i = 0; i < guesses; i++){
+     let number = prompt("Guess to enter 1-10");
+     if (number == 9){
+         console.log("You may enter");
+         break;
+     }else{
+         console.log("Better luck next time ;)");
+     }
+ }
